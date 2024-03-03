a20978972b5590ca207d1cb3bee8f6982f0ac03b
Touka kefi <kefitouka2@gmail.com>
2024-03-03 19:10:32 +0100

    first commit

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+<section>
+
+    
+</section>
+
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/Theme/DarkTheme.js
@@ -0,0 +1,29 @@
+const { createTheme } = require ("@mui/material");
+const { dark } = require("@mui/material/styles/createPalette");
+
+const darkTheme=createTheme({
+
+palette:{
+
+    mode:dark,
+    primary:{
+        main:"#e91e63"
+    },
+    secondary:{
+        main:""
+    },
+    black:{
+        main:"#242B2E"
+    },
+    
+}
+
+
+}
+
+
+
+
+
+)
+ 
